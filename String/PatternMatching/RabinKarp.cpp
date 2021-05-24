8215e639bf0a747e03afe24df91270b833a437de
AnshitaRaghuvanshi <75525942+AnshitaRaghuvanshi@users.noreply.github.com>
2021-05-24 15:57:01 +0530

    Create RabinKarp.cpp

---
+++ String/PatternMatching/RabinKarp.cpp
@@ -0,0 +1,46 @@
+//RabinKarp Pattern Searching
+//TC O((n-m+1)*m)  AS O(1)
+#include <bits/stdc++.h>
+using namespace std;
+#define d 256
+const int q=101;
+void RabinKarp(string pat,string txt){
+    int m=pat.length();
+    int n=txt.length();
+    //Compute (d^(m-1))%q
+    int h=1;
+    for(int i=1;i<=m-1;i++)
+        h=(h*d)%q;
+
+    //Compute p and to
+    int p=0,t=0;
+    for(int i=0;i<m;i++){
+        p=(p*d+pat[i])%q;
+        t=(t*d+txt[i])%q;
+    }
+
+    for(int i=0;i<=(n-m);i++){
+       //Check for hit
+       if(p==t){
+           bool flag=true;
+           for(int j=0;j<m;j++)
+                if(txt[i+j]!=pat[j]){flag=false;break;}
+            if(flag==true)cout<<i<<" ";
+       }
+       //Compute ti+1 using ti
+       if(i<n-m){
+           t=((d*(t-txt[i]*h))+txt[i+m])%q;
+        if(t<0)t=t+q;
+       }
+    }
+
+}
+ 
+int main()
+{
+    string txt = "abdabcbabc";
+    string pat="abc";
+    RabinKarp(pat,txt);
+
+    return 0;
+}
