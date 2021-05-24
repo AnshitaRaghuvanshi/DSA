817463269423def7258f0a866a26007ce6e53200
AnshitaRaghuvanshi <75525942+AnshitaRaghuvanshi@users.noreply.github.com>
2021-05-24 15:06:11 +0530

    Create Naive.cpp

---
+++ String/PatternMatching/Naive.cpp
@@ -0,0 +1,28 @@
+//Naive Pattern Searching Algo
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void NaivePatSearch(string txt, string pat)
+{
+  int n=txt.length();
+  int m=pat.length();
+  // for every i match txt with pat for atmost m length
+  //if match is not found set j back to 0 and increment i else print i
+  for(int i=0;i<=n-m;i++){
+    int j;
+    for(j=0;j<m;j++){
+      if(txt[i+j]!=pat[j])
+      break;
+    if(j==m)
+    cout<<i<<" ";
+  }
+}
+int main()
+{
+  string txt="abcabcdabcd";
+  string pat="abcd";
+
+  NaivePatSearch(txt,pat);
+  return 0;
+}
